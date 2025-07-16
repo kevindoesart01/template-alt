3b7a5c5892ca12717eeaa9a29c72aa847498195e
Kevin McGrath <kevindoesart@gmail.com>
2025-07-16 14:30:02 -0700

    Clean up prototypes and convert to starter template

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Elizabeth's prototypes",
-  description: "The home for all my prototypes",
+  title: "Next.js starter template",
+  description: "Blank template for a new Next.js project",
   icons: {
     icon: [
       {
